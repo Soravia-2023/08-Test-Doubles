e96ffbd9cb4a9673135ef6ba6bd79f50853e1a04
heidererkatrin <katrin.heiderer@nagarro.com>
2023-09-12 10:04:32 +0200

    add solution

--- CSharpCore/CSharpCoreTest/SystemTest.cs
+++ CSharpCore/CSharpCoreTest/SystemTest.cs
@@ -9,26 +9,52 @@ namespace CSharpCore.Test
         [Fact]
         public void NewlyCreatedSystem_hasNoLoggedInUsers()
         {
+            System sut = new System(new AuthorizerDummy(), null);
+
+            sut.LoginCount().Should().Be(0);
         }
 
         [Fact]
         public void OneUserIsLoggedIn_afterSuccessfulAuthorization()
         {
+            System sut = new System(new AcceptingAuthorizerStub(), null);
+            //Act
+            sut.PerformLogin("username", "password");
+
+            sut.LoginCount().Should().Be(1);
+
         }
 
         [Fact]
         public void ErrorMailIsSent_forInvalidUserName()
         {
+            MailServiceMock mock = new MailServiceMock();
+            mock.ExpectFailureMail(System.ErrorInvalidUserName);
+            System sut = new System(new RejectingAuthorizerStub(), mock);
+            //Act
+            sut.PerformLogin("", "password");
+            mock.Verify();
         }
 
         [Fact]
         public void ErrorMailIsSent_onFailedLoginAttempt()
         {
+            MailServiceSpy spy = new MailServiceSpy();
+            System sut = new System(new RejectingAuthorizerStub(), spy);
+            //Act
+            sut.PerformLogin("username", "password");
+
+            sut.LoginCount().Should().Be(0);
+            spy.FailedLoginUserName.Should().Be("username");
         }
 
         [Fact]
         public void KnownUser_canLogin()
         {
+            System sut = new System(new AuthorizerFake(), null);
+
+            sut.PerformLogin("jimmy", "password");
+            sut.LoginCount().Should().Be(1);
         }
     }
 }
